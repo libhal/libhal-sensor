6b521ae55482385fc2f7e5e49de72e3c09e026da
MaliaLabor <99438964+MaliaLabor@users.noreply.github.com>
2024-08-09 16:57:07 +0000

    [create-pull-request] automated change

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <libhal-__device__/__device__.hpp>
+#include <libhal-sensor/sensor.hpp>
 
 int main()
 {
-  hal::__device__::__device___replace_me bar;
+  hal::sensor::sensor_replace_me bar;
 }
